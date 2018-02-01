eccba3785caef427a6b5f79c3363957dfbbd2016
Michael Bayne <mdb@samskivert.com>
2018-02-01 14:33:15 -0800

    Basic Haskell mode with syntax highlighting.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Scaled Haskell Mode
+
+A [Scaled] major editing mode for editing Haskell code.
+
+Documentation TBD.
+
+## Distribution
+
+Scaled Haskell Mode is released under the New BSD License. The most recent version of the code is
+available at http://github.com/scaled/haskell-mode
+
+[Scaled]: https://github.com/scaled/scaled
---
+++ src/scala/scaled/haskell/HaskellMode.scala
@@ -0,0 +1,60 @@
+//
+// Scaled Haskell Mode - a Scaled major mode for editing Haskell code
+// http://github.com/scaled/haskell-mode/blob/master/LICENSE
+
+package scaled.haskell
+
+import scaled._
+import scaled.code.Indenter
+import scaled.grammar._
+import scaled.code.{CodeConfig, Commenter}
+
+@Plugin(tag="textmate-grammar")
+class HaskellGrammarPlugin extends GrammarPlugin {
+  import EditorConfig._
+  import CodeConfig._
+
+  override def grammars = Map("source.haskell" -> "Haskell.ndf")
+
+  override def effacers = List(
+    effacer("comment.line", commentStyle),
+    effacer("comment.block.string", stringStyle),
+    effacer("comment.line.triple-bar", docStyle),
+    effacer("constant", constantStyle),
+    effacer("invalid", warnStyle),
+    effacer("keyword.directive", moduleStyle),
+    effacer("keyword", keywordStyle),
+    effacer("string", stringStyle),
+    effacer("variable", variableStyle),
+    effacer("entity.name.function", functionStyle),
+    effacer("entity.name.type.module", moduleStyle),
+    effacer("entity.name.type", typeStyle),
+    effacer("support.other.module", moduleStyle),
+    effacer("storage", variableStyle)
+  )
+}
+
+@Major(name="haskell",
+       tags=Array("code", "project", "haskell"),
+       pats=Array(".*\\.hs"),
+       desc="A major mode for editing Haskell code.")
+class HaskellMode (env :Env) extends GrammarCodeMode(env) {
+
+  override def dispose () {} // nada for now
+
+  override def langScope = "source.haskell"
+
+  override def keymap = super.keymap.
+    bind("self-insert-command", "'"); // don't auto-pair single quote
+
+  // HACK: leave indent as-is
+  override def computeIndent (row :Int) :Int = Indenter.readIndent(buffer, Loc(row, 0))
+
+  override val commenter = new Commenter {
+    override def linePrefix  = "--"
+    override def blockOpen = ""
+    override def blockClose = ""
+    override def blockPrefix = ""
+    override def docPrefix   = "|||"
+  }
+}
